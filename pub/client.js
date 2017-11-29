19d7577b2e2530fe2518d53b53cfaaaa83c3921e
Joel Whalen <jowhalen13@gmail.com>
2017-11-29 12:56:46 -0500

    Fixed socket.io

--- pub/client.js
+++ pub/client.js
@@ -1,5 +1,8 @@
-var socket = io.connect('/');
-socket.on('onconnected', function( data ) {
-//Note that the data is the object we sent from the server, as is. So we can assume its id exists. 
-	console.log( 'Connected successfully to the socket.io server. My server side ID is ' + data.id );
+var socket = io();
+
+$(function() {
+	socket.on('onconnected', function( data ) {
+		//Note that the data is the object we sent from the server, as is. So we can assume its id exists. 
+		console.log( 'Connected successfully to the socket.io server. My server side ID is ' + data.id );
+	});
 });
--- server.js
+++ server.js
@@ -1,63 +1,59 @@
-// app code has been adapted from a template we found here:
-// http://buildnewgames.com/real-time-multiplayer/
-var 
+//This part is the same as usual...
+var express = require("express");
+var app = express();
+
+var http = require("http");
+
+//We are getting an instance of a Node HTTP (web) server here.
+//We are also telling it to connect up with our Express application,
+//so it can handle requests.
+var server = http.Server(app);
+
+//On command prompt, we need to do "npm install socket.io"
+var socketio = require("socket.io");
+
+//instantiates our 'io' instance, and also connects it up with the HTTP
+//server we already created.
+var io = socketio(server);
+
+//Just for static files (like usual).  Eg. index.html, client.js, etc.
+app.use(express.static("pub"));
+var UUID 			= require('node-uuid');
+var gameport		= process.env.PORT || 4004;
+
+var
 	Eet 			= require("./src/Eet.js"),
 	Player 			= require("./src/Player.js"),
-	GameBoard 		= require("./src/GameBoard.js"),
+	GameBoard 		= require("./src/GameBoard.js");
 
-	gameport		= process.env.PORT || 4004,
-
-	socketio		= require('socket.io'),
-	http 			= require("http"),
-	express			= require('express'),
-	UUID 			= require('node-uuid'),
-	bodyParser 		= require("body-parser"),
-
-	verbose			= false,
-	app 			= express(),
-	server 			= http.Server(app);
-	io 				= socketio(server);
-	
-
-app.use(express.static("pub")); //static files served up (like index.html)
-app.use(bodyParser.urlencoded({extended: true})); //we can use req.body
-
-//This handler will listen for requests on /*, any file from the root of our app.
-//See expressjs documentation for more info on routing.
-app.get( '/*' , function( req, res, next ) {
-
-	//This is the current file they have requested
-	var file = req.params[0]; 
-
-	//For debugging, we can track what files are requested.
-	if(verbose) console.log('\t :: Express :: file requested : ' + file);
-
-	//Send the requesting client the file.
-	res.sendFile( __dirname + '/' + file );
-
-}); //app.get *
-
-//Socket IO
-io.on('connection', function (client) {
-
+//Every time a client connects (visits the page) this function(socket) {...} gets executed.
+//The socket is a different object each time a new client connects.
+io.on("connection", function(socket) {
+	console.log("Somebody connected.");
 	//Generate a new UUID, looks something like 
 	//5b2ca132-64bd-4513-99da-90e838ca47d1
 	//and store this on their socket/connection
-	client.userid = UUID();
+	socket.userid = UUID();
 
 	//tell the player they connected, giving them their id
-	client.emit('onconnected', { id: client.userid } );
+	socket.emit('onconnected', { id: socket.userid } );
 
 	//Useful to know when someone connects
-	console.log('\t socket.io:: player ' + client.userid + ' connected');
+	console.log('\t socket.io:: player ' + socket.userid + ' connected');
 
 	//When this client disconnects
-	client.on('disconnect', function () {
-		console.log('\t socket.io:: client disconnected ' + client.userid );
+	socket.on('disconnect', function () {
+		console.log('\t socket.io:: client disconnected ' + socket.userid );
 	});
 
+	var player1 = new Player("Joel", socket.userid);
+
+	console.log("Your name is " + player1.name + ", and your id is " + player1.id);
 });
 
-app.listen( gameport );
-//Log something so we know that it succeeded.
-console.log('\t :: Express :: Listening on port ' + gameport );
+server.listen(gameport, function() {
+	console.log("Server is listening on port " + gameport);
+});
+
+
+
